0d92cbfe282a70fc0f705ceab0aba22beb08665a
Antony Polukhin <antoshkka@gmail.com>
2025-05-13 18:06:31 +0300

    minor polishings and more tests

--- test/implicit_convert.cpp
+++ test/implicit_convert.cpp
@@ -54,5 +54,16 @@ int main() {
     Value value_inf(std::numeric_limits<double>::infinity());
     BOOST_TEST_EQ(boost::lexical_cast<std::string>(value_inf), "inf");
 
+    // iostreams have default precision 6, see [basic.ios.cons]
+    const double precision_more_than_6_digits = 1.23456789123;
+    // At the moment we do not set the precision for types that use conversion
+    // operators for ostreaming, so the below test pass. We do not set precision
+    // for user provided `operator<<`, types with conversion operators follow
+    // the same design decision for now.
+    BOOST_TEST_NE(
+        boost::lexical_cast<std::string>(Value(precision_more_than_6_digits)),
+        boost::lexical_cast<std::string>(precision_more_than_6_digits)
+    );
+
     return boost::report_errors();
 }
