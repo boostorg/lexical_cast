fce2528378bf3919bd60df3df649445cdfe37fd5
Antony Polukhin <antoshkka@gmail.com>
2025-05-13 14:26:51 +0300

    Do not permit implicit conversions to floating point types in decltype(shl_real_type(x, buffer)) substitution (fixes #82)

--- test/implicit_convert.cpp
+++ test/implicit_convert.cpp
@@ -8,8 +8,6 @@
 
 #include <boost/core/lightweight_test.hpp>
 
-#include <boost/lexical_cast.hpp>
-
 struct oops {
     operator int () const {
         return 41;
@@ -20,9 +18,27 @@ inline std::ostream& operator<<(std::ostream& os, const oops&) {
     return os << 42;
 }
 
-int main () {
+struct Value {
+    explicit Value(double x) : x(x) {}
+
+    operator double() const {
+        return x;
+    }
+
+    double x;
+};
+
+int main() {
     auto val = boost::lexical_cast<int>(oops{});
     BOOST_TEST_EQ(val, 42);
+
+    Value value(128);
+    auto s = boost::lexical_cast<std::string>(value);
+    BOOST_TEST_EQ(s, "128");
+
+    Value value2(128128128128128);
+    auto s2 = boost::lexical_cast<std::string>(value2);
+    BOOST_TEST_EQ(s2, "128128128128128");
 
     return boost::report_errors();
 }
